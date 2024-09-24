baf01eb0b1d0c56226acd883307798bd7e422033
7vTr0x7 <7vTrox7@gmail.com>
2024-09-24 12:07:38 +0530

    portfolio

---
+++ src/data/commons.js
@@ -0,0 +1,192 @@
+export const Bio = {
+  name: "Salman Shiakh",
+  roles: ["Frontend Developer", "Full Stack Developer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/7vTr0x7",
+  resume:
+    "https://drive.google.com/file/d/1s1M03T6dvUlV9QBXyGtMOPHCdbC1lJgx/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/salmanshaikh-/",
+};
+
+export const experiences = [
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Junior Developer",
+    company: "Open Investment Group",
+    date: "May 2023 - Apr 2024",
+    desc: "I played a role in developing Solidity-based smart contracts for real-time trading of fractional shares. Integrated React.js and ethers.js to enhance user experience.",
+    skills: [
+      "Blockchain",
+      "Smart Contracts",
+      "Web3",
+      "Ethereum",
+      "Solidity",
+      "Hardhat",
+      "Defi",
+      "ethers.js",
+      "JavaScript",
+      "React.js",
+      "Tailwind CSS",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+    collage: "savitribai phule pune university",
+    date: "Oct 2021 - may 2024",
+
+    degree: "Bachelor of Computer Applications",
+  },
+];
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: "data:image/svg+xml;base64,...",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image: "data:image/png;base64,...",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Blockchain",
+    skills: [
+      {
+        name: "Blockchain",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/0c/Blockchain_logo.png",
+      },
+      {
+        name: "Smart Contracts",
+        image:
+          "https://miro.medium.com/v2/resize:fit:600/format:webp/1*FvDbV--puB6wMg4a6hx5RQ.png",
+      },
+      {
+        name: "Web3",
+        image: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Web3.png",
+      },
+      {
+        name: "Ethereum",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/6f/Ethereum_logo_2014.svg",
+      },
+      {
+        name: "Solidity",
+        image: "https://soliditylang.org/images/solidity_logo.png",
+      },
+      {
+        name: "Hardhat",
+        image: "https://hardhat.org/static/logo.png",
+      },
+      {
+        name: "DeFi",
+        image:
+          "https://cryptologos.cc/logos/decentralized-finance-defi-logo.png",
+      },
+      {
+        name: "ethers.js",
+        image: "https://docs.ethers.io/v5/img/ethers-logo.png",
+      },
+    ],
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "TweetIT",
+    date: "Sep 2024 - Sep 2024",
+    description:
+      "A Twitter clone that replicates core functionalities such as posting, liking, commenting, following, and bookmarking. Developed a scalable social media platform with user profiles, real-time post interactions, and media upload support.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "React Js",
+      "Node Js",
+      "Express Js",
+      "MongoDB",
+      "Redux-toolkit",
+      "CSS",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/7vTr0x7/TweetIT",
+    webapp: "https://tweet-it-frontend.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "MyCart",
+    date: "Aug 2024 - Aug 2024",
+    description:
+      "An e-commerce website designed to offer product browsing, shopping cart, and wishlist functionality. Implemented a full-stack solution with a focus on product management and a smooth user experience for adding products to the cart and managing orders.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+    tags: [
+      "React Js",
+      "Node Js",
+      "Express Js",
+      "MongoDB",
+      "Redux-toolkit",
+      "CSS",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/7vTr0x7/MyCart",
+    webapp: "https://my-cart-frontend.vercel.app/",
+  },
+];
+
+export const TimeLineData = [{ year: 2023, text: "Started my journey" }];
